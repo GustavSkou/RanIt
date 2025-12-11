c421ed1e72527e10f5d5a2a1f6b33e4d5d490ce9
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-12-11 18:43:29 +0100

    users view

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,8 +1,28 @@
-<x-body>
-<x-nav></x-nav>
+@vite(['resources\css\users.css'])
 
-@foreach ($users as $user)
-    <h1>{{ $user->name }}</h1>
-@endforeach
+<x-body>
+    <x-nav></x-nav>
+
+    <div class="list">
+        @foreach ($users as $user)
+            <div class="user-container">
+                <image 
+                    src={{ Storage::disk('public')->exists('storage' . $user->profile_picture_path) ? asset('storage/' . $user->profile_picture_path ) : asset('images/' . $user->profile_picture_path ) }}
+                    class="profile-icon"
+                    alt="{{ Auth::user()->name }}">
+
+                <div>
+                    <h1>{{ $user->name }}</h1>
+                    <p>location</p>
+                </div>
+
+                <form action="" method="post">
+                    <button>
+                        Follow
+                    </button>
+                </form>
+            </div>
+        @endforeach
+    </div>  
 
 </x-body>
